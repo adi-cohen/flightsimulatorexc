e5e7068df9173fe6ea8656cd9dc11f221cf1a7d4
adi <adidc10@gmail.com>
2019-12-19 20:43:51 +0200

    new lexer

--- whileCommand.h
+++ whileCommand.h
@@ -6,11 +6,18 @@
 #define FLIGHTSIMULATOR_WHILECOMMAND_H
 
 #include "Command.h"
+#include "SymbolTable.h"
+#include "Interpreter.h"
 
 class whileCommand : public Command {
 public:
-    int execute(vector<string> strings, map<string, Var*> varMap, map<string, Var*> simMap, int index);
-
+    int execute(vector<string> stringVector,SymbolTable* symTable, int index, int scope);
+    bool isOperator(string s);
+    string doubleToString(double calc);
+    int isOperatorFlag(string s);
+    void updateVarMap(const SymbolTable *symTable, const string &expLeft, const string &expRight,
+                      Interpreter *arithmeticLeft,
+                      Interpreter *arithmeticRight);
 };
 
 
